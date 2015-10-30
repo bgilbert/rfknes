5fa5d23667e3bee1ad944148ddea7e2049eabe3b
Benjamin Gilbert <bgilbert@backtick.net>
2015-10-30 01:24:56 -0400

    Add routine to print an NKI

--- src/rfk.asm
+++ src/rfk.asm
@@ -28,9 +28,6 @@
 nmi_done	.byte ?
 .send
 
-.strings hello, 2, ["Hello,", "NES"]
-.string world, 2, "world."
-
 .section fixed
 start	.proc
 	.cp #$80, PPUCTRL ; configure PPU; enable NMI
@@ -45,9 +42,13 @@ start	.proc
 	cpx #$30
 	bne -
 
-	; hello world
-	.print NAMETABLE_0, 11, 13, hello_bank, hello_addr
-	.print NAMETABLE_0, 15, 14, world_bank, world_addr
+	; print NKI
+	.cp #<690, tempA
+	.cp #>690, tempA + 1
+	coord_addr = NAMETABLE_0 + 2 * 32 + 2
+	.cp #<coord_addr, tempB
+	.cp #>coord_addr, tempB + 1
+	jsr print_nki
 
 	; reset scroll after update
 	bit PPUSTATUS	; clear address latch
@@ -82,5 +83,31 @@ nmi	.proc
 
 irq	.proc
 	rti
+	.pend
+
+; Print an NKI
+; tempA - NKI number
+; tempB - nametable address
+print_nki .proc
+	asl tempA	; get table offset by doubling nki_num: low
+	rol tempA + 1	; and high
+	.cp #<nki_table, tempC ; copy low byte of table base to tempC.L
+	lda #>nki_table	; load high byte
+	clc		; clear carry
+	adc tempA + 1	; add high byte of table offset
+	sta tempC + 1	; and store it to tempC.H
+	ldy tempA	; get low byte of offset for indirect indexed
+	lda (tempC),y	; load low byte of string address
+	sta tempA	; store to tempA.L
+	iny		; increment offset for entry.H
+	lda (tempC),y	; load entry.H
+	tax		; copy to X
+	lsr		; shift to recover high bits of string address
+	lsr
+	ora #$80	; fix the top two bits
+	sta tempA + 1	; and store to tempA.H
+	txa		; get entry.H back again
+	and #$03	; and extract the bank
+	jmp do_print	; tail call
 	.pend
 .send
