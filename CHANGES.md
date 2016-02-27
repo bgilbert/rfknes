d8669595dadb16c2d50b821c1fcabf57b35a373a
Benjamin Gilbert <bgilbert@backtick.net>
2016-02-26 21:23:33 -0500

    Update for release

--- CHANGES.md
+++ CHANGES.md
@@ -1,3 +1,7 @@
+# Version 1.1.701
+
+- Add NROM build (for copying onto a donor cartridge)
+
 # Version 1.0.701
 
 - Initial release
--- src/rfk.asm
+++ src/rfk.asm
@@ -26,7 +26,7 @@
 .include "nki.asm"
 .include "nmi.asm"
 
-VERSION = "1.0"
+VERSION = "1.1"
 
 ; Debug options
 INDICATE_KITTEN = 0	; use smiley-face glyph for kitten
